981e966e1522eaf4ea33acc9d17a4c4b510d5c10
Alyson Basilio <alyson.basilio@gmail.com>
2020-07-04 15:09:11 -0300

    Fix test config

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :class_management_api, ClassManagementApi.Repo,
   username: "postgres",
   password: "postgres",
   database: "class_management_api_test",
-  hostname: "localhost",
+  hostname: "postgres",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,

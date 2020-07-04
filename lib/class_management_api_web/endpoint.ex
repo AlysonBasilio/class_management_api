5b66d135d310de31471a38b02930b03db93c243d
Alyson Basilio <alyson.basilio@gmail.com>
2020-07-04 15:09:30 -0300

    Remove unnecessary socket

--- lib/class_management_api_web/endpoint.ex
+++ lib/class_management_api_web/endpoint.ex
@@ -10,10 +10,6 @@ defmodule ClassManagementApiWeb.Endpoint do
     signing_salt: "s2viWgwO"
   ]
 
-  socket "/socket", ClassManagementApiWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest

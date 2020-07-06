c051d73b912bb326493e947570ca06287583d412
Alyson Basilio <alyson.basilio@gmail.com>
2020-07-05 22:16:36 -0300

    Fix get_class_students test

--- test/class_management_api/classes_test.exs
+++ test/class_management_api/classes_test.exs
@@ -60,7 +60,7 @@ defmodule ClassManagementApi.ClassesTest do
     test "get_class_students!/1 returns the class with given all it students" do
       class = class_fixture()
       class_with_students = Classes.get_class_with_students!(class.id)
-      student = Users.create_student(%{name: "Bruce Wayne"})
+      {:ok, student} = Users.create_student(%{name: "Bruce Wayne"})
       Classes.add_student_to_class(class_with_students, student)
       class_students = Classes.get_class_students!(class.id)
       assert [student] == class_students
